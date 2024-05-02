20fb9d9ae10a78777f56bb38ae0b171b494b10af
Juan Camilo Developer <juan.camilorodrigez@gmail.com>
2024-05-02 16:10:54 -0500

    Configuracion para la auth desde api

---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request): JsonResponse
+    {
+        $request->authenticate();
+        $user = auth()->user();
+        $token = $request->user()->createToken('authToken')->plainTextToken;
+
+        return response()->json([
+           'message' =>'logeado exitosamente',
+            'acces_token' => $token,
+            'user' => $user,
+        ]);
+    }
+
+    public function register(Request $request): JsonResponse
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'message' =>'Registrado exitosamente',
+            'user' => $user,
+        ]);
+    }
+}
